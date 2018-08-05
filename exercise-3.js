860c43da7800a254e8e836e6c614b2a45cc7d045
DuhAbduh <muhamad.abduh.muh@gmail.com>
2018-08-05 17:58:21 +0700

    fix exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,24 +1,23 @@
-var peran = 'penyihir';
+var nama = 'Andi';
+var peran = 'ksatria';
 
-if (nama == '') {
+if (nama == '' && peran == '') {
 	console.log('Nama harus diisi!');
 } else if (nama != '' && peran == '') {
 	console.log('Halo, ' + nama + ' Pilih peranmu untuk memulai game!');
-} else if (nama != '' && peran == 'ksatria') {
+} else {
 	console.log('Selamat datang di Dunia Proxyta, ' + nama);
-	console.log(
-		'Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!'
-	);
-} else if (nama != '' && peran == 'tabib') {
-	console.log('Selamat datang di Dunia Proxyta, ' + nama);
-	console.log(
-		'Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.'
-	);
-} else if (nama != '' && peran == 'penyihir') {
-	console.log('Selamat datang di Dunia Proxyta, ' + nama);
-	console.log(
-		'Halo Penyihir ' +
-			nama +
-			', cipatakan keajaiban yang membantu kemenanganmu!'
-	);
+
+	if(nama != '' && peran == 'ksatria') {
+
+		console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+
+	} else if (nama != '' && peran == 'tabib') {
+		
+		console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
+
+	} else if (nama != '' && peran == 'penyihir') {
+		
+		console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+	}
 }
